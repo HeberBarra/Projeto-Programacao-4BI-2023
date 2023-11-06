33f47e0e3289dfa05d1cf2dc6e7d25760544ed58
HeberBarra <heberferreirabarra@gmail.com>
2023-11-05 23:00:43 -0300

    começo do código para manipulação de tarefas

--- src/main/java/Tarefa/CriarTarefa.java
+++ src/main/java/Tarefa/CriarTarefa.java
@@ -1,2 +1,18 @@
+package Tarefa;
+
 public class CriarTarefa {
+
+    private String nome;
+
+    public CriarTarefa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = String.join(" ", novoNome.strip().split(" "));
+    }
 }
