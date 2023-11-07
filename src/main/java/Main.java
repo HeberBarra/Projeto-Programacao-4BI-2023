38e4193dd0e16f3d31d5afba1d5a8a5f05280f25
HeberBarra <heberferreirabarra@gmail.com>
2023-11-07 10:39:12 -0300

    adicionei TODOs para indicar o que ainda precisa ser feito. Pode haver mais coisas entretanto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
 public class Main {
+    // TODO: criar classe de controle principal
 
     public static void main(String[] args) {
+        Main main = new Main();
+    }
+
+    // TODO: método que mostrar todas as tarefas em forma de lista,
+    //  mostrando apenas o nome, o status, o prazo e a data de entrega
+    public void mostrarTarefas() {
 
     }
 
+    // TODO: método para gerenciar uma tarefa, mostra as informações necessárias
+    //  (nome, status, funcionários, recursos e resíduos com as informações deles, prazo e data de entrega)
+    //  permitir modificação das informações usando eventos(essa parte se quiser posso fazer separado)
+    public void gerenciarTarefa() {
+
+    }
+
+    // TODO: mostra um menu de opções, que contará com opções numa caixa de diálogo para ver
+    //  tarefas, configuraçõese sair do programa
+    //  0 - ver tarefas
+    //  1 - configurações
+    //  2 - sair do programa
+    public int menuOpcoes() {
+        // return só pra não dá erro, mude
+        return 0;
+    }
 }
---
+++ src/main/java/Tarefa/GerenciarTarefas.java
@@ -0,0 +1,38 @@
+package Tarefa;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class GerenciarTarefas {
+    // TODO: criar classe para o gerenciamento de tarefas, listagem, exclução, gerenciamento, etc.
+
+    public ArrayList<String> lerArquivosTarefas() {
+        // TODO: método que lê os arquivos do diretório/pasta src/main/resources/tarefas/
+        //  e retorna objetos Tarefa com base nas informações salvas.
+
+        // return só pra não dá erro, mude
+        return new ArrayList<>();
+    }
+
+    public void excluirTarefa(String nomeTarefa) {
+        // TODO: deleta o arquivo de uma tarefa com base no nome da tarefa.
+        //  Pede confirmação ao usuário antes de deletar
+    }
+
+    public Tarefa pegarTarefaPeloNome(String nomeTarefa) {
+        // TODO: método que procura uma tarefa no diretório tarefas
+        //  e retorna um objeto Tarefa se a tarefa existir
+        //  se a Tarefa não existir criar um erro avisando
+
+        // return só pra não dá erro, mude
+        return new Tarefa("placeholder", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), LocalDate.now());
+    }
+
+    public int pegarTarelaPeloNome(String nomeTarefa, ArrayList<Tarefa> tarefas) {
+        // TODO: método alternativo que ao invés de procurar no disco,
+        //  procura num ArrayList de tarefas e retorna o index
+
+        // return só pra não dá erro, mude
+        return 0;
+    }
+}
