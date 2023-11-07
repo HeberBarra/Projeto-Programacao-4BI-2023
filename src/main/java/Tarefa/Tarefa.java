b21acc18e5a1561f45362efc85407dea7b12a17a
Heber F Barra <heberferrirabarra@gmail.com>
2023-11-07 15:40:04 -0300

    atualização para criar Tarefa apartir de arquivo JSON

--- src/main/java/Tarefa/Tarefa.java
+++ src/main/java/Tarefa/Tarefa.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,12 +22,86 @@ public class Tarefa {
     private ArrayList<Residuo> residuos;
     private LocalDate dataDeEntrega;
 
-    public Tarefa(String nome, ArrayList<String> funcionarios, ArrayList<Recurso> materiais, ArrayList<Residuo> residuos, LocalDate dataDeEntrega) {
+    public Tarefa(String nome, ArrayList<String> funcionarios, ArrayList<Recurso> recursos, ArrayList<Residuo> residuos, LocalDate dataDeEntrega) {
         this.nome = nome;
         this.funcionarios = funcionarios;
-        this.recursos = materiais;
+        this.recursos = recursos;
         this.residuos = residuos;
         this.dataDeEntrega = dataDeEntrega;
+    }
+
+    public Tarefa(String caminhoArquivo, JSONObject tarefaJSON) {
+        String[] partesCaminho = caminhoArquivo.split("/");
+        String nomeArquivo = partesCaminho[partesCaminho.length - 1];
+        this.nome = nomeArquivo.substring(0, nomeArquivo.length() - 5);
+
+        System.out.println(tarefaJSON);
+        List<String> chavesTarefa = Arrays.stream(new String[]{"status", "funcionarios", "recursos", "residuos", "dataDeEntrega"}).toList();
+        List<String> chavesMaterial = Arrays.stream(new String[]{"nome", "valor", "quantidade"}).toList();
+        ArrayList<String> chavesRecurso = new ArrayList<>(List.copyOf(chavesMaterial));
+        chavesRecurso.add(1, "fornecedor");
+        ArrayList<String> chavesResiduo = new ArrayList<>(List.copyOf(chavesMaterial));
+        chavesResiduo.add(1, "localDiscarte");
+
+        HashMap<String, Status> statusHashMap = new HashMap<>();
+        statusHashMap.put("EM_ANDAMENTO", Status.EM_ANDAMENTO);
+        statusHashMap.put("CONCLUIDO", Status.CONCLUIDO);
+        statusHashMap.put("ATRASADO", Status.ATRASADO);
+        statusHashMap.put("NAO_CONFORME", Status.NAO_CONFORME);
+        statusHashMap.put("REPROVADO", Status.REPROVADO);
+
+        for (var key: tarefaJSON.keySet()) {
+            var valorJson = tarefaJSON.get(key);
+
+            switch (key) {
+
+                case "status" -> status = statusHashMap.get((String) valorJson);
+
+                case "funcionarios" -> {
+                    ArrayList<String> funcionarios = new ArrayList<>();
+
+                    for (var funcionario: (JSONArray) valorJson) {
+                        funcionarios.add((String) funcionario);
+                    }
+
+                    this.funcionarios = funcionarios;
+                }
+
+                case "recursos" -> {
+                    ArrayList<Recurso> recursos = new ArrayList<>();
+                    for (int i = 0; i < ((JSONArray) valorJson).length(); i++) {
+                        JSONObject recurso = (JSONObject) ((JSONArray) valorJson).get(i);
+                        String nome = (String) recurso.get(chavesRecurso.get(0));
+                        String fornecedor = (String) recurso.get(chavesRecurso.get(1));
+                        long valor = (int) recurso.get(chavesRecurso.get(2));
+                        int quantidade = (int) recurso.get(chavesRecurso.get(3));
+
+                        recursos.add(new Recurso(nome, fornecedor, valor, quantidade));
+                    }
+
+                    this.recursos = recursos;
+                }
+
+                case "residuos" -> {
+                    ArrayList<Residuo> residuos = new ArrayList<>();
+                    for (int i = 0; i < ((JSONArray) valorJson).length(); i++) {
+                        JSONObject residuo = (JSONObject) ((JSONArray) valorJson).get(i);
+                        String nome = (String) residuo.get(chavesResiduo.get(0));
+                        String localDescarte = (String) residuo.get(chavesResiduo.get(1));
+                        long valor = (int) residuo.get(chavesResiduo.get(2));
+                        int quantidade = (int) residuo.get(chavesResiduo.get(3));
+                        residuos.add(new Residuo(nome, localDescarte, valor, quantidade));
+                    }
+
+                    this.residuos = residuos;
+                }
+
+                case "dataDeEntrega" -> {
+                    String[] partesData = ((String) valorJson).split("-");
+                    this.dataDeEntrega = LocalDate.of(Integer.parseInt(partesData[0]), Integer.parseInt(partesData[1]), Integer.parseInt(partesData[2]));
+                }
+            }
+        }
     }
 
     // salvas todas as informações essencias da tarefa, numa estrutura JSON
